79d2bb358e2833be992fc0fce22a942ff6d5a046
Kyle Estacio <anyalovepeanutsss@gmail.com>
2025-10-22 01:40:42 +0800

    full  user UI

---
+++ User/Check Out.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace FLOWSHOPINV
+{
+    partial class Check_Out
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Check_Out));
+            this.atcpanel = new System.Windows.Forms.Panel();
+            this.summaryco = new System.Windows.Forms.Panel();
+            this.cohourlbl = new System.Windows.Forms.Label();
+            this.esthrlbl = new System.Windows.Forms.Label();
+            this.codatelbl = new System.Windows.Forms.Label();
+            this.pudatelbl = new System.Windows.Forms.Label();
+            this.puoddpb = new System.Windows.Forms.PictureBox();
+            this.puordlbl = new System.Windows.Forms.Label();
+            this.pricecolbl = new System.Windows.Forms.Label();
+            this.prodsublbl = new System.Windows.Forms.Label();
+            this.ordersummlbl = new System.Windows.Forms.Label();
+            this.z = new System.Windows.Forms.PictureBox();
+            this.addpb = new System.Windows.Forms.PictureBox();
+            this.coitemtb = new System.Windows.Forms.TextBox();
+            this.pobutton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.copanel = new System.Windows.Forms.Panel();
+            this.pricecotb = new System.Windows.Forms.TextBox();
+            this.desccolbl = new System.Windows.Forms.Label();
+            this.coitemlbl = new System.Windows.Forms.Label();
+            this.contactTB = new System.Windows.Forms.TextBox();
+            this.contactlbl = new System.Windows.Forms.Label();
+            this.atcpanel.SuspendLayout();
+            this.summaryco.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.puoddpb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.z)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.addpb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.copanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // atcpanel
+            // 
+            this.atcpanel.BackColor = System.Drawing.Color.Transparent;
+            this.atcpanel.Controls.Add(this.contactlbl);
+            this.atcpanel.Controls.Add(this.contactTB);
+            this.atcpanel.Controls.Add(this.summaryco);
+            this.atcpanel.Controls.Add(this.z);
+            this.atcpanel.Controls.Add(this.addpb);
+            this.atcpanel.Controls.Add(this.coitemtb);
+            this.atcpanel.Controls.Add(this.pobutton);
+            this.atcpanel.Controls.Add(this.panel1);
+            this.atcpanel.Controls.Add(this.pictureBox1);
+            this.atcpanel.Controls.Add(this.copanel);
+            this.atcpanel.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.atcpanel.Location = new System.Drawing.Point(1, -3);
+            this.atcpanel.Name = "atcpanel";
+            this.atcpanel.Size = new System.Drawing.Size(1462, 705);
+            this.atcpanel.TabIndex = 1;
+            // 
+            // summaryco
+            // 
+            this.summaryco.Controls.Add(this.cohourlbl);
+            this.summaryco.Controls.Add(this.esthrlbl);
+            this.summaryco.Controls.Add(this.codatelbl);
+            this.summaryco.Controls.Add(this.pudatelbl);
+            this.summaryco.Controls.Add(this.puoddpb);
+            this.summaryco.Controls.Add(this.puordlbl);
+            this.summaryco.Controls.Add(this.pricecolbl);
+            this.summaryco.Controls.Add(this.prodsublbl);
+            this.summaryco.Controls.Add(this.ordersummlbl);
+            this.summaryco.Location = new System.Drawing.Point(489, 358);
+            this.summaryco.Name = "summaryco";
+            this.summaryco.Size = new System.Drawing.Size(402, 191);
+            this.summaryco.TabIndex = 9;
+            // 
+            // cohourlbl
+            // 
+            this.cohourlbl.AutoSize = true;
+            this.cohourlbl.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cohourlbl.ForeColor = System.Drawing.Color.Black;
+            this.cohourlbl.Location = new System.Drawing.Point(303, 157);
+            this.cohourlbl.Name = "cohourlbl";
+            this.cohourlbl.Size = new System.Drawing.Size(80, 21);
+            this.cohourlbl.TabIndex = 14;
+            this.cohourlbl.Text = "3-4 Hours";
+            // 
+            // esthrlbl
+            // 
+            this.esthrlbl.AutoSize = true;
+            this.esthrlbl.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.esthrlbl.Location = new System.Drawing.Point(5, 157);
+            this.esthrlbl.Name = "esthrlbl";
+            this.esthrlbl.Size = new System.Drawing.Size(124, 22);
+            this.esthrlbl.TabIndex = 13;
+            this.esthrlbl.Text = "Estimated Hours";
+            // 
+            // codatelbl
+            // 
+            this.codatelbl.AutoSize = true;
+            this.codatelbl.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codatelbl.ForeColor = System.Drawing.Color.Black;
+            this.codatelbl.Location = new System.Drawing.Point(317, 129);
+            this.codatelbl.Name = "codatelbl";
+            this.codatelbl.Size = new System.Drawing.Size(54, 21);
+            this.codatelbl.TabIndex = 12;
+            this.codatelbl.Text = "Today";
+            // 
+            // pudatelbl
+            // 
+            this.pudatelbl.AutoSize = true;
+            this.pudatelbl.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pudatelbl.Location = new System.Drawing.Point(4, 129);
+            this.pudatelbl.Name = "pudatelbl";
+            this.pudatelbl.Size = new System.Drawing.Size(42, 22);
+            this.pudatelbl.TabIndex = 11;
+            this.pudatelbl.Text = "Date";
+            // 
+            // puoddpb
+            // 
+            this.puoddpb.Image = ((System.Drawing.Image)(resources.GetObject("puoddpb.Image")));
+            this.puoddpb.Location = new System.Drawing.Point(117, 95);
+            this.puoddpb.Name = "puoddpb";
+            this.puoddpb.Size = new System.Drawing.Size(20, 21);
+            this.puoddpb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.puoddpb.TabIndex = 10;
+            this.puoddpb.TabStop = false;
+            // 
+            // puordlbl
+            // 
+            this.puordlbl.AutoSize = true;
+            this.puordlbl.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puordlbl.Location = new System.Drawing.Point(4, 94);
+            this.puordlbl.Name = "puordlbl";
+            this.puordlbl.Size = new System.Drawing.Size(115, 22);
+            this.puordlbl.TabIndex = 3;
+            this.puordlbl.Text = "Pick up Order";
+            // 
+            // pricecolbl
+            // 
+            this.pricecolbl.AutoSize = true;
+            this.pricecolbl.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pricecolbl.ForeColor = System.Drawing.Color.Crimson;
+            this.pricecolbl.Location = new System.Drawing.Point(321, 65);
+            this.pricecolbl.Name = "pricecolbl";
+            this.pricecolbl.Size = new System.Drawing.Size(62, 21);
+            this.pricecolbl.TabIndex = 2;
+            this.pricecolbl.Text = "₱ 2,500";
+            this.pricecolbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // prodsublbl
+            // 
+            this.prodsublbl.AutoSize = true;
+            this.prodsublbl.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prodsublbl.Location = new System.Drawing.Point(4, 51);
+            this.prodsublbl.Name = "prodsublbl";
+            this.prodsublbl.Size = new System.Drawing.Size(137, 22);
+            this.prodsublbl.TabIndex = 1;
+            this.prodsublbl.Text = "Product Subtotal";
+            // 
+            // ordersummlbl
+            // 
+            this.ordersummlbl.AutoSize = true;
+            this.ordersummlbl.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ordersummlbl.Location = new System.Drawing.Point(4, 3);
+            this.ordersummlbl.Name = "ordersummlbl";
+            this.ordersummlbl.Size = new System.Drawing.Size(133, 22);
+            this.ordersummlbl.TabIndex = 0;
+            this.ordersummlbl.Text = "Order Summary";
+            // 
+            // z
+            // 
+            this.z.Image = ((System.Drawing.Image)(resources.GetObject("z.Image")));
+            this.z.Location = new System.Drawing.Point(908, 123);
+            this.z.Name = "z";
+            this.z.Size = new System.Drawing.Size(27, 29);
+            this.z.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.z.TabIndex = 7;
+            this.z.TabStop = false;
+            // 
+            // addpb
+            // 
+            this.addpb.Image = ((System.Drawing.Image)(resources.GetObject("addpb.Image")));
+            this.addpb.Location = new System.Drawing.Point(829, 125);
+            this.addpb.Name = "addpb";
+            this.addpb.Size = new System.Drawing.Size(27, 25);
+            this.addpb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.addpb.TabIndex = 6;
+            this.addpb.TabStop = false;
+            // 
+            // coitemtb
+            // 
+            this.coitemtb.Location = new System.Drawing.Point(825, 123);
+            this.coitemtb.Name = "coitemtb";
+            this.coitemtb.Size = new System.Drawing.Size(112, 28);
+            this.coitemtb.TabIndex = 5;
+            // 
+            // pobutton
+            // 
+            this.pobutton.BackColor = System.Drawing.Color.Crimson;
+            this.pobutton.Location = new System.Drawing.Point(455, 591);
+            this.pobutton.Name = "pobutton";
+            this.pobutton.Size = new System.Drawing.Size(454, 36);
+            this.pobutton.TabIndex = 3;
+            this.pobutton.Text = "Place Order  ";
+            this.pobutton.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightPink;
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1465, 60);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(352, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // copanel
+            // 
+            this.copanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.copanel.Controls.Add(this.pricecotb);
+            this.copanel.Controls.Add(this.desccolbl);
+            this.copanel.Controls.Add(this.coitemlbl);
+            this.copanel.Location = new System.Drawing.Point(355, 112);
+            this.copanel.Name = "copanel";
+            this.copanel.Size = new System.Drawing.Size(599, 106);
+            this.copanel.TabIndex = 8;
+            this.copanel.Paint += new System.Windows.Forms.PaintEventHandler(this.copanel_Paint);
+            // 
+            // pricecotb
+            // 
+            this.pricecotb.BackColor = System.Drawing.Color.LightPink;
+            this.pricecotb.Location = new System.Drawing.Point(492, 55);
+            this.pricecotb.Name = "pricecotb";
+            this.pricecotb.Size = new System.Drawing.Size(81, 28);
+            this.pricecotb.TabIndex = 2;
+            // 
+            // desccolbl
+            // 
+            this.desccolbl.AutoSize = true;
+            this.desccolbl.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.desccolbl.Location = new System.Drawing.Point(163, 53);
+            this.desccolbl.Name = "desccolbl";
+            this.desccolbl.Size = new System.Drawing.Size(288, 20);
+            this.desccolbl.TabIndex = 1;
+            this.desccolbl.Text = "A dozen red roses wrapped in black matte";
+            // 
+            // coitemlbl
+            // 
+            this.coitemlbl.AutoSize = true;
+            this.coitemlbl.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coitemlbl.Location = new System.Drawing.Point(164, 18);
+            this.coitemlbl.Name = "coitemlbl";
+            this.coitemlbl.Size = new System.Drawing.Size(125, 26);
+            this.coitemlbl.TabIndex = 0;
+            this.coitemlbl.Text = "Eternal Rose";
+            // 
+            // contactTB
+            // 
+            this.contactTB.Location = new System.Drawing.Point(524, 296);
+            this.contactTB.Multiline = true;
+            this.contactTB.Name = "contactTB";
+            this.contactTB.Size = new System.Drawing.Size(330, 20);
+            this.contactTB.TabIndex = 10;
+            // 
+            // contactlbl
+            // 
+            this.contactlbl.AutoSize = true;
+            this.contactlbl.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contactlbl.Location = new System.Drawing.Point(523, 263);
+            this.contactlbl.Name = "contactlbl";
+            this.contactlbl.Size = new System.Drawing.Size(333, 18);
+            this.contactlbl.TabIndex = 3;
+            this.contactlbl.Text = "Kindly provide your email address for further contact.";
+            // 
+            // Check_Out
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1460, 699);
+            this.Controls.Add(this.atcpanel);
+            this.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Check_Out";
+            this.Text = "Check_Out";
+            this.atcpanel.ResumeLayout(false);
+            this.atcpanel.PerformLayout();
+            this.summaryco.ResumeLayout(false);
+            this.summaryco.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.puoddpb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.z)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.addpb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.copanel.ResumeLayout(false);
+            this.copanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel atcpanel;
+        private System.Windows.Forms.Button pobutton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox coitemtb;
+        private System.Windows.Forms.PictureBox addpb;
+        private System.Windows.Forms.PictureBox z;
+        private System.Windows.Forms.Panel copanel;
+        private System.Windows.Forms.Label desccolbl;
+        private System.Windows.Forms.Label coitemlbl;
+        private System.Windows.Forms.TextBox pricecotb;
+        private System.Windows.Forms.Panel summaryco;
+        private System.Windows.Forms.Label ordersummlbl;
+        private System.Windows.Forms.Label prodsublbl;
+        private System.Windows.Forms.Label pricecolbl;
+        private System.Windows.Forms.Label puordlbl;
+        private System.Windows.Forms.PictureBox puoddpb;
+        private System.Windows.Forms.Label codatelbl;
+        private System.Windows.Forms.Label pudatelbl;
+        private System.Windows.Forms.Label cohourlbl;
+        private System.Windows.Forms.Label esthrlbl;
+        private System.Windows.Forms.Label contactlbl;
+        private System.Windows.Forms.TextBox contactTB;
+    }
+}
